71febb8fe66d08e811b63b1956138bf62c8e7e48
ydodobara <ydodo@uw.edu>
2021-11-17 16:05:37 -0800

    file for aggregate table

---
+++ aggregate_table.R
@@ -0,0 +1,3 @@
+library(tidyverse)
+
+unemployment <- read.csv("https://raw.githubusercontent.com/info201b-2021-aut/final-project-ydodobara/main/unemployment_data_us.csv?token=AV5HNBNQS2G3EMLZ3X2UWQLBT3JP2")
