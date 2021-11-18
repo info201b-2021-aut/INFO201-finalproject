2a0d0b9d7a43d33bc00e14805fec365067edf79c
ydodobara <ydodo@uw.edu>
2021-11-18 15:42:44 -0800

    created a table grouped by date comparing unemployment rates of different races

--- aggregate_table.R
+++ aggregate_table.R
@@ -1,3 +1,7 @@
 library(tidyverse)
 
 unemployment <- read.csv("https://raw.githubusercontent.com/info201b-2021-aut/final-project-ydodobara/main/unemployment_data_us.csv?token=AV5HNBNQS2G3EMLZ3X2UWQLBT3JP2")
+
+unemployment_by_race <- unemployment %>%
+  group_by(Date) %>%
+  select(Date, White, Black, Asian, Hispanic)
