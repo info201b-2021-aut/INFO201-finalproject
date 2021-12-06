fa54135b20209ced1692443793ce23b4583c1a4f
ydodobara <ydodo@uw.edu>
2021-12-05 23:09:26 -0800

    finished interactive page 3

--- app.R
+++ app.R
@@ -1,3 +1,16 @@
+#install.packages("shinyWidgets")
+library(shiny)
+library(tidyverse)
+library(plotly)
+library(shinyWidgets)
+
+unemployment <- read.csv("https://raw.githubusercontent.com/info201b-2021-aut/final-project-ydodobara/main/unemployment_data_us.csv?token=AV5HNBIXZBOMVUYCBJUB4D3BW2RJ4")
+
+# This changes month abbreviations into numbers (Jan = 1, etc.) and creates a column called "Gender" with values from both "Men" and "Women" columns
+unemployment <- unemployment %>%
+  mutate(Month = match(unemployment$Month, month.abb)) %>%
+  pivot_longer(cols = c("Men", "Women"), names_to = "Gender", values_to = "Rate by Gender") 
+
 # Intro page
 
 # Interactive page 1
@@ -8,12 +21,57 @@
 
 # Interactive page 3
 
+unemployment_date_gender <- tabPanel(
+  "Unemployment by Date and Gender",
+  sidebarLayout(
+    sidebarPanel(
+      checkboxGroupInput(
+        "gender",
+        label = h3("Select one or more genders"),
+        choices = list("Men" = "Men", "Women" = "Women")
+      ),
+      radioGroupButtons(
+        "years",
+        label = h3("Select a range of years"),
+        choices = unique(unemployment$Year)
+      )
+    ),
+    mainPanel(
+      plotlyOutput(outputId = "date_gender_chart"),
+      p("Economic change can change unemployment rates, and analyzing the unemployment rates in a certain month and/or year 
+      can describe the economic situation the U.S. was in during that time. In addition, unemployment rates by gender are important
+        to analyze due to the fact that women are known to be treated less fairly than men.")
+    )
+  )
+)
 
 # Conclusion page
 
 
 # UI code
 
+ui <- navbarPage(
+  "Unemployment Data Analysis",
+  unemployment_date_gender
+)
 
 # Server code
+
+server <- function(input, output){
+  output$date_gender_chart <- renderPlotly({
+    unemployment %>%
+      filter(Year %in% input$years) %>%
+      filter(Gender %in% input$gender) %>%
+      group_by(Gender) %>%
+      plot_ly(x = ~Month, y = ~`Rate by Gender`, type = "scatter", mode = "lines",
+              text = ~paste("Month:", Month, "Year:", Year, "Rate:", `Rate by Gender`), 
+              color = ~Gender) %>%
+      layout(title = "Unemployment Rates by Date and Gender",
+             xaxis = list(title = "Month (in numerical form)"),
+             yaxis = list(title = "Unemployment Rate"))
+    
+  })
+}
+
+shinyApp(ui = ui, server = server)
 
